d6d9c589c685cdbf51b2987c486fe45fc320ef7e
Edwin Körner <edwin.korner@medieinstitutet.se>
2021-11-25 14:31:06 +0100

    Fixat med hamburger menyn och lagt in lite design

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+window.onload = main;
 
+function main(){
+    addEventListeners();
+}
+
+
+function addEventListeners(){
+    const menuButton = document.getElementById("hamburger-checkbox");
+    menuButton.onclick = hamburgerToggle;
+}
+
+function hamburgerToggle(){
+    const menuList = document.getElementById("menu-list");
+    const fadedBackground = document.getElementById("menu-opacity");
+
+    if (menuList.style.width === "30rem"){
+        fadedBackground.style.width = null;
+        menuList.style.width = null;
+    } else {
+        menuList.style.width = "30rem";
+        fadedBackground.style.width="100%";
+    }
+}
