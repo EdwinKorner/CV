1c932c82b81c6825414fcfd50c6bcb650c074a1b
Edwin Körner <edwin.korner@medieinstitutet.se>
2021-11-30 20:47:39 +0100

    Har fixat massa saker :)

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ function main(){
 function addEventListeners(){
     const menuButton = document.getElementById("hamburger-checkbox");
     menuButton.onclick = hamburgerToggle;
+
     let menuText = document.getElementById("list-text");
     menuText.onclick = hamburgerToggle;
 }
-
+//Hamburger-meny
 function hamburgerToggle(){
     let line1 = document.getElementById("line1");
     let line2 = document.getElementById("line2");
@@ -25,19 +26,59 @@ function hamburgerToggle(){
     }else {
         menuList.style.width = "30rem";
         fadedBackground.style.width="100%";
+        document.querySelector("body").onclick = hamburgerToggle;
     }
      
 }
 
-const observer = new IntersectionObserver (entries => {
+//Animationer onScroll
 
-});
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
 
-observer.observe(document.querySelector('#om-mig'));
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+  };
+  
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
 
-entries.forEach( entry => {
-    const div = entry.target.querySelector
-    if(entry.isIntersecting) {
-        entry.target.classList.add('enter-sight');
+  //Formulär animationer
+
+  
+    const submitbutton = document.getElementById("button");
+    const form = document.querySelector("form");
+    const kontaktH2 = document.getElementById("kontakt-h2");
+    submitbutton.onclick = submit;
+    
+    
+    
+
+    
+    function submit(){
+        form.style.display="none";
+        kontaktH2.style.display="block";
+        let förnamn = document.getElementById("förnamn").value;
+        document.getElementById("name").innerHTML = förnamn;
     }
-});
